46ffec469c50b87e36dfdd98052cda0f500c59f7
jameshsu1125 <jameshsu1125@gmail.com>
2022-11-21 21:54:36 +0800

    Update constant.js

--- src/settings/constant.js
+++ src/settings/constant.js
@@ -71,10 +71,8 @@ export const IMAGE_STATE = { base64: '' };
 export const splitText = (str, size) => {
 	const numChunks = Math.ceil(str.length / size);
 	const chunks = new Array(numChunks);
-
 	for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
 		chunks[i] = str.substr(o, size);
 	}
-
 	return chunks;
 };
